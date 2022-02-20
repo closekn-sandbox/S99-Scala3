268e971f8903022632e68cd89f2e0661792a0a83
closekn <close.me.kn@gmail.com>
2022-02-20 22:30:11 +0900

    Add ScalaTest & Try P01

---
+++ src/main/scala/WorkingWithLists/P01.scala
@@ -0,0 +1,7 @@
+package WorkingWithLists
+
+object P01:
+  def last[T](list : List[T] ) : T = list match
+    case x :: Nil => x
+    case x :: xs => last(xs)
+    case _ => sys.error("Last is not defined.")
---
+++ src/test/scala/WorkingWithLists/Test01.scala
@@ -0,0 +1,10 @@
+package WorkingWithLists
+
+import org.scalatest.funspec.AnyFunSpec
+
+class Test01 extends AnyFunSpec:
+  describe("P01") {
+    it("Example") {
+      assert(P01.last(List (1, 1, 2, 3, 5, 8)) == 8)
+    }
+  }
