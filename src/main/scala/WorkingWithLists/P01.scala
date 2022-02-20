fb140973bd0e374c569d490d17ea77e3bdbd961e
closekn <close.me.kn@gmail.com>
2022-02-20 22:52:58 +0900

    Add scalafmt

--- src/main/scala/WorkingWithLists/P01.scala
+++ src/main/scala/WorkingWithLists/P01.scala
@@ -1,7 +1,7 @@
 package WorkingWithLists
 
 object P01:
-  def last[T](list : List[T] ) : T = list match
+  def last[T](list: List[T]): T = list match
     case x :: Nil => x
-    case x :: xs => last(xs)
-    case _ => sys.error("Last is not defined.")
+    case x :: xs  => last(xs)
+    case _        => sys.error("Last is not defined.")
--- src/test/scala/WorkingWithLists/Test01.scala
+++ src/test/scala/WorkingWithLists/Test01.scala
@@ -5,6 +5,6 @@ import org.scalatest.funspec.AnyFunSpec
 class Test01 extends AnyFunSpec:
   describe("P01") {
     it("Example") {
-      assert(P01.last(List (1, 1, 2, 3, 5, 8)) == 8)
+      assert(P01.last(List(1, 1, 2, 3, 5, 8)) == 8)
     }
   }
